fb5cdd5637063f6734f352a5c0732e4514b55a51
rebeccawmx <rebecca.wmx@outlook.com>
2018-09-18 19:33:17 +0800

    write day4.py

--- day4.py
+++ day4.py
@@ -7,9 +7,16 @@
 # 第二步：调用time函数的的tm_yday显示天数
 # 第三步：输出天数
 
+
 import time
 
 
+# 第一步：输入年月日
+
 Date = input("请输入年月日，格式为YYYYMMDD:")
+
+# 第二步：调用time函数的的tm_yday显示天数
 days = time.strptime(Date, '%Y%m%d').tm_yday
+
+# 第三步：输出天数
 print("这一天在这一年的 {}".format(days))
